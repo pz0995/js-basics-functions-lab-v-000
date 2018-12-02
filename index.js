7f20711c9c8d2bd7f12614a56c4392a8ef2f1dd8
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-02 11:17:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {
+  if distanceFromHqInBlocks(43)
+   return 1
+ else if distanceFromHqInBlocks(50)
+   return 8
+ else
+   return 8
+}
+
+function distanceFromHqInFeet(someValue) {
+   distanceFromHqInBlocks(someValue);
+}
+
+function distanceTravelledInFeet() {
+  return 264 * distanceFromHqInBlocks
+}
+
+function calculatesFarePrice(start, destination) {
+  if (feet < 400) {
+    return free;
+  }
+  else if (feet 400 < && < 2000) {
+    return ((feet - 400) *0.02)
+  }
+  else if (feet 2000 < && < 2500) {
+    return 25
+  }
+  else
+    return "cannot travel that far"
+  }
+}
